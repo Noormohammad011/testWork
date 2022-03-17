20be34e9c19420ce3b3c7c9bbc29e2ede4c43189
Noormohammad011 <noormohammad.net1503011@gmail.com>
2022-03-17 17:42:30 +0600

    'testWork'

--- src/App.js
+++ src/App.js
@@ -5,68 +5,89 @@ import './App.css';
 
 
 function App() {
+
+
   const [count, setCount] = useState(false)
+
+
+  const celsiusToFahrenheit = (celsius) => ((celsius * 9) / 5 + 32).toFixed(2)
+
+  const fahrenheitToCelsius = (fahrenheit) => (((fahrenheit - 32) * 5) / 9).toFixed(2)
+
+  const check = (celsius) => {
+    if (celsius > 30) {
+      return `hot`
+    } else if (celsius < 15) {
+      return 'cold'
+    } else if (celsius >= 15 && celsius < 30) {
+      return 'sunny'
+    }
+    else {
+      return 'not given'
+    }
+  }
     const data = [
-  {
-    id: '1',
-    date: '10-02-03',
-    temparature: '30',
-  },
-  {
-    id: '2',
-    date: '10-03-03',
-    temparature: '14',
-  },
-  {
-    id: '3',
-    date: '10-03-03',
-    temparature: '16',
-  },
-  {
-    id: '4',
-    date: '10-04-03',
-    temparature: '16',
-  },
-  {
-    id: '5',
-    date: '10-05-03',
-    temparature: '31',
-  },
-]
+      {
+        id: '1',
+        date: '10-02-03',
+        temparature: '30',
+      },
+      {
+        id: '2',
+        date: '10-03-03',
+        temparature: '14',
+      },
+      {
+        id: '3',
+        date: '10-03-03',
+        temparature: '16',
+      },
+      {
+        id: '4',
+        date: '10-04-03',
+        temparature: '16',
+      },
+      {
+        id: '5',
+        date: '10-05-03',
+        temparature: '-2',
+      },
+    ]
 
   return (
     <div className='App'>
       <button onClick={() => setCount(!count)}>
         {' '}
-        {count ? 'Celcius' : 'Farengite'}
+        {count ? 'Celcius' : 'Farenhite'}
       </button>
 
       {count
         ? data?.map((x) => (
             <>
               <div key={x.id}>
-                <h1>{x.date}</h1>
-                <h2>{x.temparature}</h2>
+                <h1>Date: {x.date}</h1>
+                <h1>Temparature:{fahrenheitToCelsius(x.temparature)}</h1>
+                <h2>
+                  Temparature Info: {check(fahrenheitToCelsius(x.temparature))}
+                </h2>
               </div>
             </>
           ))
-        : 'false'}
+        : data?.map((x) => (
+            <>
+              <div key={x.id}>
+                <h1>Date:{x.date}</h1>
+                <h1>Temparature:{celsiusToFahrenheit(x.temparature)}</h1>
+                <h2>
+                  {' '}
+                  Temparature Info:{check(celsiusToFahrenheit(x.temparature))}
+                </h2>
+              </div>
+            </>
+          ))}
     </div>
   )
 }
-const celsiusToFahrenheit = (celsius) => (celsius * 9) / 5 + 32
 
-const fahrenheitToCelsius = (fahrenheit) => ((fahrenheit - 32) * 5) / 9
-
-const chekc = (celsius) => { 
-  if(celsius > 30) {
-    return 'hot'
-  }else if(celsius < 15){
-    return 'cold'
-  }else if(celsius >= 15 && celsius < 30){
-    return 'sunny'
-  }
-
- }
 
 export default App;
